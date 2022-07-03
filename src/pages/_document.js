0e2a853eded74197229fcfc3fa8c33dace63a77a
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-07-03 15:54:28 +0700

    chore: add favicon for web manifest

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,12 +11,23 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link
-            rel='preload'
-            href='/fonts/inter-var-latin.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
           />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
           <script
             async
             defer
