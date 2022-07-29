cd44f613802ba7c182710325496394cbd3b989e3
afif <afif@garasilabs.com>
2022-07-29 21:15:51 +0700

    :lipstick: style: update chakra ui's theme configuration

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,32 +1,69 @@
 import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
-const theme = extendTheme({
-  colors: {
-    github: {
-      50: '#f5f5f5',
-      100: '#ebebeb',
-      200: '#cccccc',
-      300: '#adadad',
-      400: '#707070',
-      500: '#333333',
-      600: '#2e2e2e',
-      700: '#262626',
-      800: '#1f1f1f',
-      900: '#191919',
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('#f9fafb', '#202023')(props),
     },
-    polywork: {
-      50: '#f5f5f5',
-      100: '#ece9fb',
-      200: '#c5bdf4',
-      300: '#9e92ed',
-      400: '#7766e6',
-      500: '#503adf',
-      600: '#3720c5',
-      700: '#1e126d',
-      800: '#120b42',
-      900: '#060416',
+  }),
+};
+
+const components = {
+  Heading: {
+    variants: {
+      'section-title': {
+        textDecoration: 'underline',
+        fontSize: 20,
+        textUnderlineOffset: 6,
+        textDecorationColor: '#525252',
+        textDecorationThickness: 4,
+        marginTop: 3,
+        marginBottom: 4,
+      },
     },
   },
-});
+  Link: {
+    baseStyle: (props) => ({
+      color: mode('#3d7aed', '#ff63c3')(props),
+      textUnderlineOffset: 3,
+    }),
+  },
+};
+
+const colors = {
+  grassTeal: '#88ccca',
+  github: {
+    50: '#f5f5f5',
+    100: '#ebebeb',
+    200: '#cccccc',
+    300: '#adadad',
+    400: '#707070',
+    500: '#333333',
+    600: '#2e2e2e',
+    700: '#262626',
+    800: '#1f1f1f',
+    900: '#191919',
+  },
+  polywork: {
+    50: '#f5f5f5',
+    100: '#ece9fb',
+    200: '#c5bdf4',
+    300: '#9e92ed',
+    400: '#7766e6',
+    500: '#503adf',
+    600: '#3720c5',
+    700: '#1e126d',
+    800: '#120b42',
+    900: '#060416',
+  },
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ colors, components, styles, config });
 
 export default theme;
