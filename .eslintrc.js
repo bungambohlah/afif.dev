b9cf388b2be4f3903c770c744135380cdcd84d89
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-07-30 15:27:08 +0700

    :construction_worker: build: add typescript-eslint/parser at eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.node.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
   extends: 'galex',
   rules: {
     'new-cap': 'off',
